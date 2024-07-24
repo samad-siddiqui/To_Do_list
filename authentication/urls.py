16b78b8d7a2936f8d84b3d54f872e3e571b27e03
BlitzkreckBoys <theasamad6@gmail.com>
2024-07-24 12:15:33 -0400

    remove venv

--- authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from authentication import views
-
-urlpatterns = [
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutPage, name='logout'), 
-]
--- registration/urls.py
+++
@@ -1,7 +0,0 @@
- 
-from django.urls import path
-from registration import views
- 
-urlpatterns = [
-    path('register/', views.signUp, name='register'),
-    ]
